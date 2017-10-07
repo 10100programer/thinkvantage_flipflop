623714d1ccf762577ba1aa4be7c37187dc25af86
10100programer <10100programer@users.noreply.github.com>
2017-10-07 02:49:33 -0500

    Create thinkflip.cpp

---
+++ thinkflip.cpp
@@ -0,0 +1,27 @@
+/* 
+Thinkvantage button fliplop software
+This program is designed to be mapped to two commands that run in a filp flop maner
+*/
+
+
+init() //to be run the first time the program runs in order to setup file structure so the logic works
+{
+
+}
+
+main()
+{
+
+}
+
+if()//
+{
+
+}
+
+if()
+{
+
+}
+
+return 0x123FF;//program has an error most likely because the init failed
