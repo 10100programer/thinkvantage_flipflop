ce561f3ce4366be5ac50fbdd0e291649592731c6
10100programer <10100programer@users.noreply.github.com>
2017-10-07 03:38:38 -0500

    Finished code need to compile and test

--- thinkflip.cpp
+++ thinkflip.cpp
@@ -6,30 +6,44 @@ This program is designed to be mapped to two commands that run in a filp flop ma
 #include <string>
 #include <fstream>
 
+bool fileExists(const char *fileName)
+{
+    ifstream infile(fileName);
+    return infile.good();
+}
 init() //to be run the first time the program runs in order to setup file structure so the logic works
 {
-
+  ofstream initfile;
+  initfile.open ("/var/lib/thinkflip/state");//make state file
+  initfile << "0"; //writes zero to file
+  initfile.close(); //closes the file
+//create
 }
 
 main()
 {
+fstream filestate;
+filestate.open("/var/lib/thinkflip/state");
 
-if()//file does not exists
+bool state = false;//false = 0 true = 1
+filestate >> state; //check to see if this will work
+
+if(fileExists("/var/lib/thinkflip/state"))//file does not exists
 {
-  init();//runs init
-  return 1;
+  init();//runs init and creates file
+  return 128;//specal return code
 }
 
-if()//file at location 0 = 0 do this
+if(state)//file at location 0 = 0 do this
 {
   system("redshift -O 2700")//runs redshift for nighttime
-  return 1;
+  return 1;//command completed successfully
 }
 
-if()//file at location 0 = 1 do this
+if(state)//file at location 0 = 1 do this
 {
   system("redshift -x")//runs redshift for nighttime
-  return 1;
+  return 1;//command completed successfully
 }
   return 0x123FF;//program has an error most likely because the init failed
 }
