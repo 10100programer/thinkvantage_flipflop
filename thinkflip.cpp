4e183af8c5527a1932b887623e13661a408c0127
10100programer <10100programer@users.noreply.github.com>
2017-10-07 03:03:02 -0500

    Update thinkflip.cpp

--- thinkflip.cpp
+++ thinkflip.cpp
@@ -2,8 +2,7 @@
 Thinkvantage button fliplop software
 This program is designed to be mapped to two commands that run in a filp flop maner
 */
-
-
+#include 
 init() //to be run the first time the program runs in order to setup file structure so the logic works
 {
 
@@ -12,16 +11,22 @@ init() //to be run the first time the program runs in order to setup file struct
 main()
 {
 
-}
-
-if()//
+if()//file does not exists
 {
-
+  init();//runs init
+  return 1;
 }
 
-if()
+if()//file at location 0 = 0 do this
 {
-
+  system("redshift -O 2700")//runs redshift for nighttime
+  return 1;
 }
 
-return 0x123FF;//program has an error most likely because the init failed
+if()//file at location 0 = 1 do this
+{
+  system("redshift -x")//runs redshift for nighttime
+  return 1;
+}
+  return 0x123FF;//program has an error most likely because the init failed
+}
