878381587c0c8831598614cd9213e4ce794890d4
10100programer <10100programer@users.noreply.github.com>
2017-10-07 16:21:55 -0500

    changed some things, rewrote others, still doesnt work

--- thinkflip.cpp
+++ thinkflip.cpp
@@ -6,15 +6,16 @@ This program is designed to be mapped to two commands that run in a filp flop ma
 #include <string>
 #include <fstream>
 
+using namespace std;
 bool fileExists(const char *fileName)
 {
     ifstream infile(fileName);
     return infile.good();
 }
-init() //to be run the first time the program runs in order to setup file structure so the logic works
+void init() //to be run the first time the program runs in order to setup file structure so the logic works
 {
   ofstream initfile;
-  initfile.open ("/var/lib/thinkflip/state");//make state file
+  initfile.open ("state");//make state file
   initfile << "0"; //writes zero to file
   initfile.close(); //closes the file
 //create
@@ -22,28 +23,55 @@ init() //to be run the first time the program runs in order to setup file struct
 
 main()
 {
-fstream filestate;
-filestate.open("/var/lib/thinkflip/state");
-
-bool state = false;//false = 0 true = 1
-filestate >> state; //check to see if this will work
-
-if(fileExists("/var/lib/thinkflip/state"))//file does not exists
+if(fileExists("state")==false)//file does not exists
 {
+cout << "Running INIT\n";
   init();//runs init and creates file
+cout <<"returned 128";
   return 128;//specal return code
 }
 
-if(state)//file at location 0 = 0 do this
+fstream filestate;
+filestate.open("state");
+
+//string conversion area
+char file;
+filestate.read(file,1);
+bool state = false;//false = 0 true = 1
+if (file[0] == 1)
 {
-  system("redshift -O 2700")//runs redshift for nighttime
+state=true;
+}
+if (file[0] == 0)
+{
+state=false;
+}
+
+//state = filestate; //check to see if this will work
+//cout >> file
+if(state == true)
+	cout << "true";
+
+if(state == false)
+	cout << "false";
+
+
+if(state == false)//file at location 0 = 0 do this
+{
+filestate << "1";
+filestate.close();
+  system("redshift -O 2700");//runs redshift for nighttime
+
+cout <<"returned 1";
   return 1;//command completed successfully
 }
 
-if(state)//file at location 0 = 1 do this
+if(state == true)//file at location 0 = 1 do this
 {
-  system("redshift -x")//runs redshift for nighttime
+  system("redshift -x");//runs redshift for nighttime
+cout <<"returned 1";
   return 1;//command completed successfully
 }
+cout <<"returned 0x123FF";
   return 0x123FF;//program has an error most likely because the init failed
 }
